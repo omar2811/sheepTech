618af5e9fb3ba5d87a13722b9b3f7df7e7908168
omarDev <omar.alberto.2811@hotmail.com>
2023-11-06 20:37:15 -0600

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       <h1>hola devs</h1>
+       <h1>inicio de algo</h1>
         </p>
         <a
           className="App-link"
